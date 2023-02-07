252ada0e33d223e54ad86f1103397a48e22ae984
Walter Schemith <wascasla@gmail.com>
2023-02-07 13:32:29 -0300

    updated some configurations

--- models/index.js
+++ models/index.js
@@ -6,7 +6,6 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.json')[env];
 const config = require(__dirname + '/../config/database.js')[env];
 const db = {};
 
@@ -15,7 +14,6 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  console.log("va por el else",{config, env})
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
