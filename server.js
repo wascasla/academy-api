99f78245f21d537a6638e4dc6f8a6e775a6ed695
Walter Schemith <wascasla@gmail.com>
2023-02-07 16:10:16 -0300

    updated some configurations to deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const app = require("./app");
 const db = require("./models");
 
-const port = 5000;
+const port = (process.env.PORT || 5000);
 const server = http.createServer(app);
 
 db.sequelize.authenticate().then(() => {
